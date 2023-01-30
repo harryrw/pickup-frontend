006602d8de542e59d5be658f19d552488abf7807
harry <harry.rw96@gmail.com>
2023-01-30 23:17:14 +1100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const styles = {
+  color: 'red',
+  fontSize: '30px'
+}
+
+const App = () => {
+  return (
+    <div style={styles}>App is running!</div>
+  )
+}
+
+export default App
