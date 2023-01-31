f4f14e29f1fa3982529e73e405af7cf90f122204
harry <harry.rw96@gmail.com>
2023-01-31 22:35:30 +1100

    updated ShowCourt component format

---
+++ src/components/CourtSelection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CourtSelection = () => {
+  return (
+    <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default CourtSelection
