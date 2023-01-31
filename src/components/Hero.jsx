a9fb37ed348ae951c6d94f961771291b4d803960
harry <harry.rw96@gmail.com>
2023-01-31 18:08:25 +1100

    updated Hero styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
-<div className="col-md-6">
-        <div className="h-100 p-5 text-white bg-dark rounded-3">
+<div className="col-md-6 w-100">
+        <div className="h-100 p-5 text-warning bg-dark text-center">
           <h2>Pick Up</h2>
           <p>Find games near you:</p>
           </div>
