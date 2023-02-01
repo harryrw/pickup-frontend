23cda240b09f72e4a0f39a3d9c50e2580f36ff16
harry <harry.rw96@gmail.com>
2023-02-01 17:20:28 +1100

    imported SearchBox to Hero component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
     <>
       <Navbar />
       <Hero />
-      <SearchBox />
       <ShowCourt />
     </>
   )
--- src/components/ShowCourt.jsx
+++ src/components/ShowCourt.jsx
@@ -2,8 +2,8 @@ import React from 'react'
   
 const ShowCourt = ({ name="Test Name", address="Fawkner Park, 24-88 Commercial Rd, South Yarra VIC 3141", img="/src/assets/court.jpg"}) => {
   return (
-    <div className="card" style={{width: '18rem'}}>
-      <img src={img} className="card-img-top" style={{height:200, width:300}} alt="..."/>
+    <div className="card" style={{width: '25rem'}}>
+      <img src={img} className="card-img-top" style={{height:'15rem', width: '25rem'}} alt="..."/>
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{address}</p>
