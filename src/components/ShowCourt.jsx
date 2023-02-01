9a1519c39708dfbe2260fa08c12f63832bafa2aa
harry <harry.rw96@gmail.com>
2023-02-01 16:19:37 +1100

    updated ShowCourt styling

--- src/components/ShowCourt.jsx
+++ src/components/ShowCourt.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
   
-const ShowCourt = ({ name="Test Name", address="Fawkner Park, 24-88 Commercial Rd, South Yarra VIC 3141"}) => {
+const ShowCourt = ({ name="Test Name", address="Fawkner Park, 24-88 Commercial Rd, South Yarra VIC 3141", img="/src/assets/court.jpg"}) => {
   return (
-      <div className="card">
-      <img src="/src/assets/court.jpg" className="card-img-top" style={{height:200, width:300}} alt="..."/>
+    <div className="card" style={{width: '18rem'}}>
+      <img src={img} className="card-img-top" style={{height:200, width:300}} alt="..."/>
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <p className="card-text">{address}</p>
