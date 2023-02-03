8643d8f68eabe9eec049985f9a293927ec641c0a
harry <harry.rw96@gmail.com>
2023-02-04 00:15:27 +1100

    added basic GameSelection grid layout for /Games

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,19 @@ import Hero from './Hero'
 import ShowCourt from './ShowCourt'
 import NewCourt from './NewCourt'
 import NewGame from './NewGame'
-import CourtSelection from './CourtSelection'
 import ShowGame from './ShowGame'
+import GameSelection from './GameSelection'
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Hero />
-      <div>
-        <p> space</p>
-      </div>
-      <ShowGame />
-      {/* <ShowCourt />
-      <NewCourt /> 
-      <NewGame /> */}
+      <GameSelection />
+      {/* <Hero /> */}
+      {/* <ShowGame /> */}
+      {/* <ShowCourt /> */}
+      {/* <NewCourt />  */}
+      {/* <NewGame />  */}
     </>
   )
 }
