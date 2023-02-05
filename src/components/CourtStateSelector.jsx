972b8d55e78cc80b29d34e48df53ade35ed2c1c2
Zac Long <zac.long.96@gmail.com>
2023-02-05 18:42:00 +1100

    cleaned up court state selector

--- src/components/CourtStateSelector.jsx
+++ src/components/CourtStateSelector.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
-const CourtStateSelector = () => {
+const stateList = [
+    { name: 'ACT', route: '/courts/state/act' },
+    { name: 'NSW', route: '/courts/state/nsw' },
+    { name: 'NT', route: '/courts/state/nt' },
+    { name: 'QLD', route: '/courts/state/qld' },
+    { name: 'SA', route: '/courts/state/sa' },
+    { name: 'TAS', route: '/courts/state/tas' },
+    { name: 'VIC', route: '/courts/state/vic' },
+    { name: 'WA', route: '/courts/state/wa' }
+  ]
+  
+  const CourtStateSelector = () => {
     return (
-        <nav aria-label="State navigation for courts">
-            <ul className="pagination flex-wrap" style={{}}>
-                <li class="page-item"><Link to={`/courts`} className="page-link text-dark bg-warning border-secondary" >All Courts</Link></li>
-                <li class="page-item bg-warning"><Link to={`/courts/state/act`} className="page-link text-dark bg-warning border-secondary" >ACT</Link></li>
-                <li class="page-item"><Link to={`/courts/state/nsw`} className="page-link text-dark bg-warning border-secondary" >NSW</Link></li>
-                <li class="page-item"><Link to={`/courts/state/nt`} className="page-link text-dark bg-warning border-secondary" >NT</Link></li>
-                <li class="page-item"><Link to={`/courts/state/qld`} className="page-link text-dark bg-warning border-secondary" >QLD</Link></li>
-                <li class="page-item"><Link to={`/courts/state/sa`} className="page-link text-dark bg-warning border-secondary" >SA</Link></li>
-                <li class="page-item"><Link to={`/courts/state/tas`} className="page-link text-dark bg-warning border-secondary" >TAS</Link></li>
-                <li class="page-item"><Link to={`/courts/state/vic`} className="page-link text-dark bg-warning border-secondary" >VIC</Link></li>
-                <li class="page-item"><Link to={`/courts/state/wa`} className="page-link text-dark bg-warning border-secondary" >WA</Link></li>
-            </ul>
-        </nav>
+      <nav aria-label="State navigation for courts">
+        <ul className="pagination flex-wrap" style={{}}>
+          <li className="page-item">
+            <Link to={`/courts`} className="page-link text-dark bg-warning border-secondary">
+              All Courts
+            </Link>
+          </li>
+          {stateList.map(state => (
+            <li className="page-item" key={state.route}>
+              <Link to={state.route} className="page-link text-dark bg-warning border-secondary">
+                {state.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     )
-    }
-
-export default CourtStateSelector
+  }
+  
+  export default CourtStateSelector
