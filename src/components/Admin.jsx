5329265779b21117fd78ba305c66b2a113b75e77
harry <harry.rw96@gmail.com>
2023-02-05 23:38:44 +1100

    updated layout and functions of admin buttons

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -4,6 +4,10 @@ const Admin = () => {
     const [password, setPassword] = useState("");
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [errorMessage, setErrorMessage] = useState("");
+    const [updateGameValue, setUpdateGameValue] = useState("");
+    const [deleteGameValue, setDeleteGameValue] = useState("");
+    const [updateCourtValue, setUpdateCourtValue] = useState("");
+    const [deleteCourtValue, setDeleteCourtValue] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -15,41 +19,49 @@ const Admin = () => {
         }
     };
 
-    const updateGame = (event) => {
+    const updateGame = () => {
+        console.log(`Update Game with value: ${updateGameValue}`);
+    };
 
-    }
+    const deleteGame = () => {
+        console.log(`Delete Game with value: ${deleteGameValue}`);
+    };
 
-    const deleteGame = (event) => {
+    const updateCourt = () => {
+        console.log(`Update Court with value: ${updateCourtValue}`);
+    };
 
-    }
-
-    const updateCourt = (event) => {
-
-    }
-
-    const deleteCourt = (event) => {
-
-    }
-
+    const deleteCourt = () => {
+        console.log(`Delete Court with value: ${deleteCourtValue}`);
+    };
 
     return (
         <div>
             {!isAuthenticated ? (
-                <>
-                    <h1> Enter Password </h1>
-                    <form onSubmit={handleSubmit}>
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                        <button type="submit">Submit</button>
-                        {errorMessage !== "" && <p style={{ color: "red" }}>{errorMessage}</p>}
-                    </form>
-                </>
+                <form onSubmit={handleSubmit}>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <button type="submit">Submit</button>
+                    {errorMessage !== "" && <p style={{ color: "red" }}>{errorMessage}</p>}
+                </form>
             ) : (
-                <div>
+                <div style={{ display: "flex", flexDirection: "column" }}>
                     <h1>With great admin power comes great admin responsibility</h1>
-                    <button onClick={updateGame}>Update Game</button>
-                    <button onClick={deleteGame}>Delete Game</button>
-                    <button onClick={updateCourt}>Update Court</button>
-                    <button onClick={deleteCourt}>Delete Court</button>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <input type="text" value={updateGameValue} onChange={(e) => setUpdateGameValue(e.target.value)} />
+                        <button onClick={updateGame}>Update Game</button>
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <input type="text" value={deleteGameValue} onChange={(e) => setDeleteGameValue(e.target.value)} />
+                        <button onClick={deleteGame}>Delete Game</button>
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <input type="text" value={updateCourtValue} onChange={(e) => setUpdateCourtValue(e.target.value)} />
+                        <button onClick={updateCourt}>Update Court</button>
+                    </div>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <input type="text" value={deleteCourtValue} onChange={(e) => setDeleteCourtValue(e.target.value)} />
+                        <button onClick={deleteCourt}>Delete Court</button>
+                    </div>
                 </div>
             )}
         </div>
