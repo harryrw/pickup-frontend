a3809f98dbe6a7c21796f843199980f74f3b7b1d
harry <harry.rw96@gmail.com>
2023-02-09 15:30:06 +1100

    updated Hero image sizing and added blurb

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,14 +7,19 @@ const Hero = () => {
       <div className="h-100 p-5 text-warning bg-dark text-center flex-column" style={{ display: 'flex', alignItems: 'center' }}>
         <h1>Pick Up</h1>
         <img src="/src/assets/hoophero.png" alt="Basketball court at sunset" style={{ borderRadius: '15px', width: '50%', height: '50%', objectFit: 'cover' }} />
-        
+        <p>
+          <br></br>
+          Looking to play local basketball games but don't know where to get started?
+          <br></br>
+          Click below to find games or courts in your area.
+        </p>
         <div style={{ display: 'flex', justifyContent: 'space-around', width: '50%', marginTop: '30px' }}>
-          <button className="text-warning" style={{ backgroundColor: '#333333', padding: '20px 40px', fontSize: '2rem', borderRadius: '10px', color: 'white', border: '2px solid #ffc107' }}>
+          <Link className="text-warning" to="/games" style={{ backgroundColor: '#333333', padding: '20px 40px', fontSize: '2rem', borderRadius: '10px', color: 'white', border: '2px solid #ffc107', textDecoration: "none" }}>
             Games
-          </button>
-          <button className="text-warning" style={{ backgroundColor: '#333333', padding: '20px 40px', fontSize: '2rem', borderRadius: '10px', color: 'white', border: '2px solid #ffc107' }}>
+          </Link>
+          <Link className="text-warning" to="/courts" style={{ backgroundColor: '#333333', padding: '20px 40px', fontSize: '2rem', borderRadius: '10px', color: 'white', border: '2px solid #ffc107', textDecoration: "none"}}>
             Courts
-          </button>
+          </Link>
         </div>
       </div>
     </div>
