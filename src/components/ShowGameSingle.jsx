a9d6e818f7321b026e9f0c44b27e7dd8d6d0a15a
harry <harry.rw96@gmail.com>
2023-02-09 17:24:34 +1100

    added styling for ShowGameSingle

--- src/components/ShowGameSingle.jsx
+++ src/components/ShowGameSingle.jsx
@@ -26,31 +26,33 @@ const ShowGameSingle = ({ game }) => {
     return (
       <>
       <Navbar />
-      <div className="card mb-3 text-warning bg-dark" style={{ maxWidth: "540px" }}>
-        <div className="row g-0">
-          <div className="col-md-6">
-            <div className="card-body">
-              <h5 className="card-title">{game.title}</h5>
-              <p className="card-text">{game.address}, {game.city}, {game.state}</p>
-              <p className="card-text">
-                <small className="card-text">Skill Level: {game.skillLevel}</small>
-              </p>
+      <div className="game-card-container" style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "80vh", fontSize:"3rem", zIndex: "2", paddingTop: "150px", paddingBottom: "150px" }}>
+        <div className="card mb-3 text-warning bg-dark" style={{ border: "2px solid #ffc107", borderRadius: "20px" }}>
+          <div className="row g-0">
+            <div className="col-md-6">
+              <div className="card-body">
+                <p className="card-title" >{game.title}</p>
+                <p className="card-text">{game.address}, {game.city}, {game.state}</p>
+                <p className="card-text">
+                  <small className="card-text">Skill Level: {game.skillLevel}</small>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="col-md-6" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <p style={{paddingTop: "1rem"}}>
-                {game.description}
-            </p>
-            <p className="card-text" style={{ padding: "0.5rem" }}>
-              Participants: {participants}
-            </p>
-            {!isClicked ? (
-              <button onClick={updateCount} href="#" className="btn btn-primary text-dark bg-warning border-secondary">
-                    Join
-                </button>
-            ) : (
-              <p>You're in the game!</p>
-              )}
+            <div className="col-md-6" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+              <p style={{paddingTop: "1rem"}}>
+                  {game.description}
+              </p>
+              <p className="card-text" style={{ padding: "0.5rem" }}>
+                Participants: {participants}
+              </p>
+              {!isClicked ? (
+                <button onClick={updateCount} href="#" className="btn btn-primary text-dark bg-warning border-secondary">
+                      Join
+                  </button>
+              ) : (
+                <p>You're in the game!</p>
+                )}
+            </div>
           </div>
         </div>
       </div>
