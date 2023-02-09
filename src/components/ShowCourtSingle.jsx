3b86a6157eb4aa611cf8e1b93751a5e16279fcdd
harry <harry.rw96@gmail.com>
2023-02-09 19:55:46 +1100

    updated ShowCourtSingle and ShowGameSingle fontsize

--- src/components/ShowCourtSingle.jsx
+++ src/components/ShowCourtSingle.jsx
@@ -5,13 +5,15 @@ const ShowCourtSingle = ({ court }) => {
   return (
     <>
     <Navbar />
-    <div className="card min-w-75 m-3 text-warning bg-dark" style={{ maxWidth: "540px" }}>
-      <div className="card-body">
-        <h5 className="card-title">{court.name}</h5>
-        <p className="card-text">{court.address}, {court.city}, {court.state}</p>
-        <p className="card-text">{court.description}</p>
-      </div>
-    </div>      
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "80vh", fontSize: "2rem" }}>
+      <div className="card min-w-75 m-3 text-warning bg-dark" style={{ border: "2px solid #ffc107", borderRadius: "20px" }}>
+        <div className="card-body">
+          <h5 className="card-title">{court.name}</h5>
+          <p className="card-text">{court.address}, {court.city}, {court.state}</p>
+          <p className="card-text">{court.description}</p>
+        </div>
+      </div>      
+    </div>
     </>
   )
 }
