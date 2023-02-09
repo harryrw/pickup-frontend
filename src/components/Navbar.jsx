4e647149d256743e996754305d5f71abb5a1be3c
harry <harry.rw96@gmail.com>
2023-02-09 12:45:17 +1100

    updated navbar button styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,31 +2,29 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-5">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-5" style={{ fontSize:"2rem"}}>
       <div className="container-fluid">
-        <Link className="navbar-brand navbar-dark text-warning " to="/">
+        <Link className="navbar-brand navbar-dark text-warning" to="/" style={{ fontSize:"2rem", float: "left" }}>
           PickUp
         </Link>
         <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            {/* <li className="nav-item">
-              <Link className="nav-link text-warning" aria-current="page" to="/games">
-                Games
-              </Link>
-            </li> */}
+          <ul className="navbar-nav" style={{display: "flex", justifyContent: "space-evenly", width: "100%"}}>
             <li className="nav-item">
-              <Link className="nav-link text-warning" to="/games">
+              <Link className="nav-link text-warning" to="/games" style={{backgroundColor: "#333333", padding: "10px 20px", border: "2px solid white", borderRadius: "20px", boxShadow: "2px 2px 10px #333"}} >
                 Games
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link text-warning" to="/courts">
+              <Link className="nav-link text-warning" to="/courts" style={{backgroundColor: "#333333", padding: "10px 20px", border: "2px solid white", borderRadius: "20px", boxShadow: "2px 2px 10px #333"}}>
                 Courts
               </Link>
             </li>
+            {/* <li style={{ float: "right" }}>
+              <img src="src/assets/basketball-orange.svg" alt="PickUp logo" style={{ width: "50px", height: "50px" }} />
+            </li> */}
           </ul>
         </div>
       </div>
