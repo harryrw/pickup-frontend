6a8ba7709a8c586a3beb6d4676c915b14645dea4
Zac Long <zac.long.96@gmail.com>
2023-04-05 11:01:15 +1000

    updated the participants counter, now sends updated participants count to API

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className="col-md-6 w-100 h-100">
       <div className="h-100 p-5 text-warning bg-dark text-center flex-column" style={{ display: 'flex', alignItems: 'center' }}>
-        <h1>Pick Up</h1>
+        <h1>PickUp</h1>
         <img src={image} alt="Basketball court at sunset" style={{ borderRadius: '15px', width: '75%', height: '75%', objectFit: 'cover' }} />
         <p style={{fontSize: "2rem"}}>
           <br></br>
